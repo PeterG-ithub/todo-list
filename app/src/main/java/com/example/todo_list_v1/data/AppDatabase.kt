8eafbb2bdf30f6ec15373d9ff7e93ab33dd8ed12
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-09-02 19:37:07 -0700

    Add AppDatabase class for task data management
    - Created AppDatabase class to manage Room database operations.
    - Included TaskDao for accessing task-related data.
    - Implemented singleton pattern with lazy initialization for efficient database access.
    - Configured Room database with fallback option for schema migrations.

---
+++ app/src/main/java/com/example/todo_list_v1/data/AppDatabase.kt
@@ -0,0 +1,26 @@
+package com.example.todo_list_v1.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var Instance: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, AppDatabase::class.java, "app_database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
