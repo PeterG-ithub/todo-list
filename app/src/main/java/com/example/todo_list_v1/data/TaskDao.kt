17af628e6e47d8dd078b9c8a258e69ab5e3e8877
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-09-02 19:31:43 -0700

    Fix error

--- app/src/main/java/com/example/todo_list_v1/data/TaskDao.kt
+++ app/src/main/java/com/example/todo_list_v1/data/TaskDao.kt
@@ -8,22 +8,20 @@ import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface TaskDao {
-    @Dao
-    interface TaskDao {
-        @Query("SELECT * FROM tasks ORDER BY dueDate ASC")
-        fun getAllTasks(): Flow<List<Task>>
+    @Query("SELECT * FROM tasks ORDER BY dueDate ASC")
+    fun getAllTasks(): Flow<List<Task>>
 
-        @Query("SELECT * FROM tasks WHERE id = :id")
-        fun getTask(id: Int): Flow<Task?>
+    @Query("SELECT * FROM tasks WHERE id = :id")
+    fun getTask(id: Int): Flow<Task?>
 
-        @Insert(onConflict = OnConflictStrategy.REPLACE)
-        suspend fun insert(task: Task)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(task: Task)
 
-        @Update
-        suspend fun update(task: Task)
+    @Update
+    suspend fun update(task: Task)
 
-        @Delete
-        suspend fun delete(task: Task)
-    }
+    @Delete
+    suspend fun delete(task: Task)
 }
