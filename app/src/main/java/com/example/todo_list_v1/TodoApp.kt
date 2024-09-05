9fbd004c41e176d20c47fcb4169a249b765e5730
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-09-05 10:06:55 -0700

    Add Preview for TodoTopAppBar

--- app/src/main/java/com/example/todo_list_v1/TodoApp.kt
+++ app/src/main/java/com/example/todo_list_v1/TodoApp.kt
@@ -1,5 +1,6 @@
 package com.example.todo_list_v1
 
+import androidx.compose.foundation.background
 import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -7,10 +8,14 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.todo_list_v1.ui.navigation.TodoNavHost
@@ -42,6 +47,17 @@ fun TodoTopAppBar(
                     )
                 }
             }
-        }
+        },
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun TodoTopAppBarPreview() {
+    TodoTopAppBar(
+        title = "My To-Do List",
+        canNavigateBack = true,
+        navigateUp = { /* Handle back navigation */ }
     )
 }
