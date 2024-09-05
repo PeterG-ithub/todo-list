4638d84abc57c36b2f8c18d6f6dcc2f0024e0883
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-09-05 10:06:39 -0700

    Update TaskEntryScreen
    - Delete description and replace with notes
    - Add Category section
    - Add Date section with example date
    - Add Time section with example time
    - Add Repeat task section with example
    - Add Alarm section
    - Add Note section textfield

--- app/src/main/java/com/example/todo_list_v1/ui/task/TaskEntryScreen.kt
+++ app/src/main/java/com/example/todo_list_v1/ui/task/TaskEntryScreen.kt
@@ -1,27 +1,53 @@
 package com.example.todo_list_v1.ui.task
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.todo_list_v1.R
 import com.example.todo_list_v1.TodoTopAppBar
@@ -29,6 +55,9 @@ import com.example.todo_list_v1.ui.AppViewModelProvider
 import com.example.todo_list_v1.ui.navigation.NavigationDestination
 import com.example.todo_list_v1.ui.theme.Todolistv1Theme
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 object TaskEntryDestination : NavigationDestination {
     override val route = "task_entry"
@@ -74,6 +103,7 @@ fun TaskEntryScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskEntryBody(
     taskUiState: TaskUiState,
@@ -81,15 +111,251 @@ fun TaskEntryBody(
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var showDatePicker by remember { mutableStateOf(false) }
+    val datePickerState = rememberDatePickerState()
+    var selectedDate by remember { mutableStateOf("") }
+
+    // Handle date change when the picker is confirmed
+    LaunchedEffect(datePickerState.selectedDateMillis) {
+        selectedDate = datePickerState.selectedDateMillis?.let {
+            convertMillisToDate(it)
+        } ?: ""
+    }
+
     Column(
-        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large))
+        modifier = modifier
+            .padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_extra_large))
     ) {
         TaskInputForm(
             taskDetails = taskUiState.taskDetails,
             onValueChange = onTaskValueChange,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
         )
+
+        Column {
+            HorizontalDivider(thickness = 1.dp, color = Color.Black)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp)
+                    .clickable(onClick = {
+
+                    })
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(horizontal = 16.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.List,
+                        contentDescription = "Select date",
+                        modifier = Modifier.padding(end = 16.dp)
+                    )
+                    Text(
+                        text = "Category",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Box(
+                        modifier = Modifier
+                            .background(
+                                color = MaterialTheme.colorScheme.primaryContainer,
+                                shape = RoundedCornerShape(8.dp),
+                                )
+                    ) {
+                        Text(
+                            text = "No Category",
+                            style = MaterialTheme.typography.bodyMedium, // Set the desired font size
+                            modifier = Modifier
+                                .padding(
+                                    horizontal = dimensionResource(id = R.dimen.padding_small), 
+                                    vertical = dimensionResource(id = R.dimen.padding_tiny))
+                        )
+                    }
+                }
+            }
+            HorizontalDivider(thickness = 1.dp, color = Color.Black)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp)
+                    .clickable(onClick = {
+                        showDatePicker = true
+                    })
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(horizontal = 16.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Default.DateRange,
+                        contentDescription = "Select date",
+                        modifier = Modifier.padding(end = 16.dp)
+                    )
+                    Text(
+                        text = selectedDate.ifEmpty { "Date" },
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Text(
+                        text = "Sun, Sep 8",
+                        style = MaterialTheme.typography.bodyMedium, // Set the desired font size
+                        modifier = Modifier
+                            .background(
+                                color = MaterialTheme.colorScheme.primaryContainer,
+                                shape = RoundedCornerShape(8.dp),
+                            )
+                            .padding(
+                                horizontal = dimensionResource(id = R.dimen.padding_small),
+                                vertical = dimensionResource(id = R.dimen.padding_tiny)
+                            )
+                    )
+
+                }
+            }
+            HorizontalDivider(thickness = 1.dp, color = Color.Black)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp)
+                    .clickable(onClick = {
+
+                    })
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(horizontal = 16.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.clock),
+                        contentDescription = "Select date",
+                        modifier = Modifier.padding(end = 16.dp)
+                    )
+                    Text(
+                        text = "Set Time",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Text(
+                        text = "7:30 AM - 9:00 AM",
+                        style = MaterialTheme.typography.bodyMedium, // Set the desired font size
+                        modifier = Modifier
+                            .background(
+                                color = MaterialTheme.colorScheme.primaryContainer,
+                                shape = RoundedCornerShape(8.dp),
+                            )
+                            .padding(
+                                horizontal = dimensionResource(id = R.dimen.padding_small),
+                                vertical = dimensionResource(id = R.dimen.padding_tiny)
+                            )
+                    )
+                }
+            }
+            HorizontalDivider(thickness = 1.dp, color = Color.Black)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp)
+                    .clickable(onClick = {
+
+                    })
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(horizontal = 16.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+
+                        painter = painterResource(id = R.drawable.repeat),
+                        contentDescription = "Select date",
+                        modifier = Modifier.padding(end = 16.dp)
+                    )
+                    Text(
+                        text = "Repeat Task",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Text(
+                        text = "Custom",
+                        style = MaterialTheme.typography.bodyMedium, // Set the desired font size
+                        modifier = Modifier
+                            .background(
+                                color = MaterialTheme.colorScheme.primaryContainer,
+                                shape = RoundedCornerShape(8.dp),
+                            )
+                            .padding(
+                                horizontal = dimensionResource(id = R.dimen.padding_small),
+                                vertical = dimensionResource(id = R.dimen.padding_tiny)
+                            )
+                    )
+                }
+            }
+
+            HorizontalDivider(thickness = 1.dp, color = Color.Black)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp)
+                    .clickable(onClick = {
+
+                    })
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(horizontal = 16.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Default.Notifications,
+                        contentDescription = "Select date",
+                        modifier = Modifier.padding(end = 16.dp)
+                    )
+                    Text(
+                        text = "Alarm",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Text(
+                        text = "Off",
+                        style = MaterialTheme.typography.bodyMedium, // Set the desired font size
+                        modifier = Modifier
+                            .background(
+                                color = MaterialTheme.colorScheme.primaryContainer,
+                                shape = RoundedCornerShape(8.dp),
+                            )
+                            .padding(
+                                horizontal = dimensionResource(id = R.dimen.padding_small),
+                                vertical = dimensionResource(id = R.dimen.padding_tiny)
+                            )
+                    )
+                }
+            }
+            HorizontalDivider(thickness = 1.dp, color = Color.Black)
+            OutlinedTextField(
+                value = "Add Note",
+                onValueChange = { },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(196.dp)
+                    .padding(top = dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+
+        if (showDatePicker) {
+            DatePickerModal(
+                onDateSelected = { date ->
+                    selectedDate = date?.let { convertMillisToDate(it) } ?: "No date selected"
+                },
+                onDismiss = { showDatePicker = false }
+            )
+        }
+
         Button(
             onClick = onSaveClick,
             enabled = taskUiState.isEntryValid,
@@ -101,6 +367,43 @@ fun TaskEntryBody(
     }
 }
 
+fun convertMillisToDate(millis: Long?): String {
+    return if (millis != null) {
+        val formatter = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
+        formatter.format(Date(millis))
+    } else {
+        "Invalid date"
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModal(
+    onDateSelected: (Long?) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState()
+
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                println("test")
+                onDateSelected(datePickerState.selectedDateMillis)
+                onDismiss()
+            }) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState)
+    }
+}
 
 @Composable
 fun TaskInputForm(
@@ -116,35 +419,16 @@ fun TaskInputForm(
         OutlinedTextField(
             value = taskDetails.name,
             onValueChange = { onValueChange(taskDetails.copy(name = it)) },
-            label = { Text(stringResource(R.string.task_name_req)) },  // Update with the appropriate label resource
+            placeholder = {Text(stringResource(R.string.task_name_placeholder))},
             colors = OutlinedTextFieldDefaults.colors(
                 focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-                unfocusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                unfocusedContainerColor = MaterialTheme.colorScheme.background,
                 disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
             ),
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
         )
-        OutlinedTextField(
-            value = taskDetails.description ?: "",
-            onValueChange = { onValueChange(taskDetails.copy(description = it)) },
-            label = { Text(stringResource(R.string.task_description_req)) },  // Update with the appropriate label resource
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-                unfocusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-                disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-            ),
-            modifier = Modifier.fillMaxWidth(),
-            enabled = enabled,
-            singleLine = true
-        )
-        if (enabled) {
-            Text(
-                text = stringResource(R.string.required_fields),
-                modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
-            )
-        }
     }
 }
 
