b40be5e6565b0878f7d69b5570745aa084e08602
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-09-10 10:44:05 -0700

    Add drop down menu functionality for the TopNav
    - Add new function MoreOptionDropdown
    - Function consist of a dropdown menu
    - Dropdown menu has custom drop down items
    - onOptionSelected is still in progress

--- app/src/main/java/com/example/todo_list_v1/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/todo_list_v1/ui/home/HomeScreen.kt
@@ -23,12 +23,18 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -42,15 +48,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -92,6 +101,9 @@ fun HomeScreen(
     // State for managing the visibility of the category entry modal
     val showCategoryEntryModal = remember { mutableStateOf(false) }
 
+    //
+    var showDropdownMenu by remember { mutableStateOf(false) }
+
     // State for managing the selected category
     val selectedCategory = remember { mutableStateOf<Category?>(null) }
 
@@ -120,10 +132,16 @@ fun HomeScreen(
                 },
                 actions = {
                     IconButton(onClick = {
-                        /* TODO */
+                        showDropdownMenu = true
                     }) {
                         Icon(Icons.Filled.MoreVert, contentDescription = stringResource(R.string.more_options))
                     }
+
+                    MoreOptionDropdown(
+                        onDismissRequest = { showDropdownMenu = false },
+                        showOptionMenu = showDropdownMenu,
+                        onOptionSelected = {}
+                    )
                 },
                 scrollBehavior = scrollBehavior
             )
@@ -215,7 +233,10 @@ private fun HomeTopAppBar(
                             color = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.primaryContainer,
                             shape = RoundedCornerShape(8.dp)
                         )
-                        .padding(horizontal = dimensionResource(id = R.dimen.padding_small), vertical = dimensionResource(id = R.dimen.padding_tiny))
+                        .padding(
+                            horizontal = dimensionResource(id = R.dimen.padding_small),
+                            vertical = dimensionResource(id = R.dimen.padding_tiny)
+                        )
                         .clickable { onCategoryClick(category) }
                         .padding(horizontal = 8.dp, vertical = 0.dp),
                     textAlign = TextAlign.Center
@@ -242,6 +263,93 @@ private fun HomeTopAppBar(
                 )
             }
         }
+    }
+}
+
+@Composable
+fun MoreOptionDropdown(
+    onDismissRequest: () -> Unit,
+    showOptionMenu: Boolean,
+    onOptionSelected: (String?) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    DropdownMenu(
+        expanded = showOptionMenu,
+        onDismissRequest = onDismissRequest,
+        modifier = modifier,
+        offset = DpOffset(x = (-8).dp, y = 0.dp)
+    ) {
+        DropdownMenuItem(
+            text = {
+                Text(
+                    text = "Add Category",
+                    color = MaterialTheme.colorScheme.primary,
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector =  Icons.Default.Add,
+                    contentDescription = null
+                )
+            },
+            onClick = {
+                onOptionSelected("Add")
+                onDismissRequest()
+            }
+        )
+        DropdownMenuItem(
+            text = {
+                Text(
+                    text = "Manage Categories",
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector =  Icons.Default.Edit,
+                    contentDescription = null
+                )
+            },
+            onClick = {
+                onOptionSelected("Manage")
+                onDismissRequest()
+            }
+        )
+        DropdownMenuItem(
+            text = {
+                Text(
+                    text = "Delete Category",
+                    color = MaterialTheme.colorScheme.error
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector =  Icons.Filled.Delete,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.error
+                )
+            },
+            onClick = {
+                onOptionSelected("Delete")
+                onDismissRequest()
+            }
+        )
+        DropdownMenuItem(
+            text = {
+                Text(
+                    text = "Sort task by",
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.sort),
+                    contentDescription = null
+                )
+            },
+            onClick = {
+                onOptionSelected("Sort")
+                onDismissRequest()
+            }
+        )
     }
 }
 
