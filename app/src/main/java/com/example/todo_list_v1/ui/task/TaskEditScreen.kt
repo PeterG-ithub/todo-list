84e1a6fefe7f6a1ea90ec50a7b5a191cb0b19b40
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-09-11 12:30:26 -0700

    Update TaskEditScreen to show TaskEntryBody

--- app/src/main/java/com/example/todo_list_v1/ui/task/TaskEditScreen.kt
+++ app/src/main/java/com/example/todo_list_v1/ui/task/TaskEditScreen.kt
@@ -1,17 +1,28 @@
 package com.example.todo_list_v1.ui.task
 
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.todo_list_v1.R
 import com.example.todo_list_v1.TodoTopAppBar
 import com.example.todo_list_v1.ui.AppViewModelProvider
 import com.example.todo_list_v1.ui.navigation.NavigationDestination
+import com.example.todo_list_v1.ui.task.entry.TaskEntryBody
+import kotlinx.coroutines.launch
 
 object TaskEditDestination : NavigationDestination {
     override val route = "task_edit"
@@ -24,24 +35,42 @@ object TaskEditDestination : NavigationDestination {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskEditScreen(
+    modifier: Modifier = Modifier,
     navigateBack: () -> Unit,
     onNavigateUp: () -> Unit,
-    modifier: Modifier = Modifier,
+    canNavigateBack: Boolean = true,
     viewModel: TaskEditViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
+    val categories by viewModel.categories.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
     Scaffold(
         topBar = {
             TodoTopAppBar(
-                title = stringResource(R.string.edit_task_title),  // Update with the appropriate title resource
-                canNavigateBack = true,
+                title = stringResource(R.string.task_edit_title),  // Update with the appropriate title resource
+                canNavigateBack = canNavigateBack,
                 navigateUp = onNavigateUp
             )
         }
-    ) { innerpadding ->
-        Text(
-            text = "Edit Task Screen",
-            modifier = modifier
-                .padding(innerpadding)
+    ) { innerPadding ->
+        TaskEntryBody(
+            taskUiState = viewModel.taskUiState,
+            categories = categories,
+            onTaskValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateTask()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(
+                    start = innerPadding.calculateStartPadding(LocalLayoutDirection.current),
+                    top = innerPadding.calculateTopPadding(),
+                    end = innerPadding.calculateEndPadding(LocalLayoutDirection.current),
+                )
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
         )
+
     }
 }
