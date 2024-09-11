f2a05d5ced0105211d8680a118ccb2773977f808
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-09-10 19:55:49 -0700

    Add TimePickerModal file
    - File contains the function for TimePickerModal
    - File contains function for AdvancedTimePickerDialog from example in
    Android Studio Time Picker Dialog
    - File contains util functions to convert time from one data type to another

---
+++ app/src/main/java/com/example/todo_list_v1/ui/task/entry/TimePickerModal.kt
@@ -0,0 +1,179 @@
+package com.example.todo_list_v1.ui.task.entry
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimeInput
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerState
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.example.todo_list_v1.R
+import com.example.todo_list_v1.ui.theme.Todolistv1Theme
+import java.util.Calendar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimePickerModal(
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit = {},
+    onConfirm: (TimePickerState) -> Unit = {},
+) {
+
+    val currentTime = Calendar.getInstance()
+
+    val timePickerState = rememberTimePickerState(
+        initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+        initialMinute = currentTime.get(Calendar.MINUTE),
+        is24Hour = false,
+    )
+
+    /** Determines whether the time picker is dial or input */
+    var showDial by remember { mutableStateOf(true) }
+
+    /** The icon used for the icon button that switches from dial to input */
+    val toggleIcon = if (showDial) {
+        painterResource(id = R.drawable.keyboard)
+    } else {
+        painterResource(id = R.drawable.clock)
+    }
+
+    AdvancedTimePickerDialog(
+        onDismiss = { onDismiss() },
+        onConfirm = { onConfirm(timePickerState) },
+        toggle = {
+            IconButton(onClick = { showDial = !showDial }) {
+                Icon(
+                    painter = toggleIcon,
+                    contentDescription = "Time picker type toggle",
+                )
+            }
+        },
+    ) {
+        if (showDial) {
+            TimePicker(
+                state = timePickerState,
+            )
+        } else {
+            TimeInput(
+                state = timePickerState,
+            )
+        }
+    }
+}
+
+@Composable
+fun AdvancedTimePickerDialog(
+    title: String = "Select Time",
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+    toggle: @Composable () -> Unit = {},
+    content: @Composable () -> Unit,
+) {
+    Dialog(
+        onDismissRequest = onDismiss,
+        properties = DialogProperties(usePlatformDefaultWidth = false),
+    ) {
+        Surface(
+            shape = MaterialTheme.shapes.extraLarge,
+            tonalElevation = 6.dp,
+            modifier =
+            Modifier
+                .width(IntrinsicSize.Min)
+                .height(IntrinsicSize.Min)
+                .background(
+                    shape = MaterialTheme.shapes.extraLarge,
+                    color = MaterialTheme.colorScheme.surface
+                ),
+        ) {
+            Column(
+                modifier = Modifier.padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 20.dp),
+                    text = title,
+                    style = MaterialTheme.typography.labelMedium
+                )
+                content()
+                Row(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .fillMaxWidth()
+                ) {
+                    toggle()
+                    Spacer(modifier = Modifier.weight(1f))
+                    TextButton(onClick = onDismiss) { Text("Cancel") }
+                    TextButton(onClick = onConfirm) { Text("OK") }
+                }
+            }
+        }
+    }
+}
+
+fun convertTimeToMillis(hour: Int, minute: Int): Long? {
+    val calendar = Calendar.getInstance()
+    calendar.set(Calendar.HOUR_OF_DAY, hour)
+    calendar.set(Calendar.MINUTE, minute)
+    calendar.set(Calendar.SECOND, 0)
+    calendar.set(Calendar.MILLISECOND, 0)
+
+    return calendar.timeInMillis
+}
+
+fun convertMillisToTime(millis: Long): Pair<Int, Int> {
+    val calendar = Calendar.getInstance()
+    calendar.timeInMillis = millis
+    val hour = calendar.get(Calendar.HOUR_OF_DAY)
+    val minute = calendar.get(Calendar.MINUTE)
+    return Pair(hour, minute)
+}
+
+fun formatTimeMillis(millis: Long): String {
+    val calendar = Calendar.getInstance().apply {
+        timeInMillis = millis
+    }
+    val hour = calendar.get(Calendar.HOUR)
+    val minute = calendar.get(Calendar.MINUTE)
+    val amPm = if (calendar.get(Calendar.AM_PM) == Calendar.AM) "AM" else "PM"
+    return String.format("%02d:%02d %s", hour, minute, amPm)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun TimePickerModalPreview() {
+    Todolistv1Theme {
+        TimePickerModal()
+    }
+}
