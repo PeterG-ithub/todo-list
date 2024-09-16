98ab24cb5d447a7b56875035f4b463dd6abcd079
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-09-16 09:15:04 -0700

    Update TaskItemCard to include next occurence
    - Add new Text under Task Name to show the next repeat occurence

--- app/src/main/java/com/example/todo_list_v1/ui/task/item/TaskItemCard.kt
+++ app/src/main/java/com/example/todo_list_v1/ui/task/item/TaskItemCard.kt
@@ -1,22 +1,33 @@
 package com.example.todo_list_v1.ui.task.item
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import com.example.todo_list_v1.R
 import com.example.todo_list_v1.data.task.Task
+import com.example.todo_list_v1.ui.task.entry.convertMillisToDate
+import com.example.todo_list_v1.ui.task.entry.convertMillisToDates
+import com.example.todo_list_v1.ui.theme.Todolistv1Theme
 
 @Composable
 fun TaskItemCard(
@@ -41,7 +52,43 @@ fun TaskItemCard(
                 }
             )
             Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.padding_small)))
-            Text(text = task.name, style = MaterialTheme.typography.bodyLarge)
+            Column {
+                Text(
+                    text = task.name,
+                    style = MaterialTheme.typography.bodyLarge,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.widthIn(max = 150.dp)
+                )
+                if (task.nextOccurrence != null) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = convertMillisToDate(task.nextOccurrence),
+                            style = MaterialTheme.typography.labelSmall,
+                            color = MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.5f)
+                        )
+
+                        Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.padding_tiny)))
+                        Icon(
+                            painter = painterResource(
+                                id = R.drawable.repeat),
+                                contentDescription = "Repeat Icon",
+                            modifier = Modifier.size(14.dp),
+                            tint = MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.5f)
+                        )
+                    }
+                }
+            }
         }
+    }
+}
+
+@Preview
+@Composable
+fun TaskItemCardPreview() {
+    Todolistv1Theme {
+        TaskItem(task = Task(name = "Preview Preview Preview Preview Preview Preview ", nextOccurrence = 1726467600000), onTaskCheckedChange = { true })
     }
 }
