feb4236202f6d1726c50494de1a5004e68554e60
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-09-16 20:07:27 -0700

    Add completedTaskRepo to AppContainer

--- app/src/main/java/com/example/todo_list_v1/data/AppContainer.kt
+++ app/src/main/java/com/example/todo_list_v1/data/AppContainer.kt
@@ -3,13 +3,17 @@ package com.example.todo_list_v1.data
 import android.content.Context
 import com.example.todo_list_v1.data.category.CategoryRepository
 import com.example.todo_list_v1.data.category.OfflineCategoryRepository
+import com.example.todo_list_v1.data.completed_task.CompletedTaskRepository
+import com.example.todo_list_v1.data.completed_task.OfflineCompletedTaskRepository
 import com.example.todo_list_v1.data.task.OfflineTasksRepository
 import com.example.todo_list_v1.data.task.TasksRepository
 
 interface AppContainer {
     val tasksRepository: TasksRepository
     val categoryRepository: CategoryRepository
+    val completedTaskRepository: CompletedTaskRepository  // New property for CompletedTaskRepository
 }
+
 
 class AppDataContainer(private val context: Context) : AppContainer {
     override val tasksRepository: TasksRepository by lazy {
@@ -18,5 +22,9 @@ class AppDataContainer(private val context: Context) : AppContainer {
 
     override val categoryRepository: CategoryRepository by lazy {
         OfflineCategoryRepository(AppDatabase.getDatabase(context).categoryDao())
+    }
+
+    override val completedTaskRepository: CompletedTaskRepository by lazy {
+        OfflineCompletedTaskRepository(AppDatabase.getDatabase(context).completedTaskDao())
     }
 }
