a4145a77effb25adecc2c0b6312a05ed75284cae
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-09-18 19:33:10 -0700

    Add new column taskCategoryId to CompletedTask
    - Add new taskCategoryId to data class completedTask
    - Add new migration 7_8

--- app/src/main/java/com/example/todo_list_v1/data/completed_task/CompletedTask.kt
+++ app/src/main/java/com/example/todo_list_v1/data/completed_task/CompletedTask.kt
@@ -3,17 +3,26 @@ package com.example.todo_list_v1.data.completed_task
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import com.example.todo_list_v1.data.category.Category
 import com.example.todo_list_v1.data.task.Task
 
 
 @Entity(
     tableName = "completed_tasks",
-    foreignKeys = [ForeignKey(
-        entity = Task::class,
-        parentColumns = ["id"],
-        childColumns = ["taskId"],
-        onDelete = ForeignKey.SET_NULL
-    )]
+    foreignKeys = [
+        ForeignKey(
+            entity = Task::class,
+            parentColumns = ["id"],
+            childColumns = ["taskId"],
+            onDelete = ForeignKey.SET_NULL
+        ),
+        ForeignKey(
+            entity = Category::class,
+            parentColumns = ["id"],
+            childColumns = ["taskCategoryId"],
+            onDelete = ForeignKey.SET_NULL
+        )
+    ]
 )
 data class CompletedTask(
     @PrimaryKey(autoGenerate = true)
@@ -22,6 +31,7 @@ data class CompletedTask(
     val taskName: String,
     val taskDescription: String? = null,
     val taskCategory: String? = null,
+    val taskCategoryId: Int? = null,
     val taskDueDate: Long? = null,
     val completedAt: Long,
 )
