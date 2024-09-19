d0a2b24d0e2b7ec6953cd6ef3169cb958b422a8d
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-09-19 13:08:39 -0700

    Update CategoryItem UI with visible and delete icons
    - Added visibility and delete icons to CategoryItem composable.

--- app/src/main/java/com/example/todo_list_v1/ui/category/CategoryItem.kt
+++ app/src/main/java/com/example/todo_list_v1/ui/category/CategoryItem.kt
@@ -1,17 +1,23 @@
 package com.example.todo_list_v1.ui.category
 
+import android.opengl.Visibility
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,15 +29,16 @@ import com.example.todo_list_v1.ui.theme.Todolistv1Theme
 fun CategoryItem(
     modifier: Modifier = Modifier,
     category: Category,
+    visible: Boolean = true,
 ) {
     Card(
-        modifier = modifier,
-        elevation = CardDefaults.cardElevation(defaultElevation = 1.dp)
+        elevation = CardDefaults.cardElevation(defaultElevation = 1.dp),
+        modifier = modifier
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(dimensionResource(id = R.dimen.padding_small)),
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
@@ -40,6 +47,19 @@ fun CategoryItem(
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
                 modifier = Modifier.widthIn(max = 225.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Icon(
+                painter = if(visible) painterResource(id = R.drawable.visible_on) else painterResource(
+                    id = R.drawable.visible_off
+                ) ,
+                contentDescription = "Visible On"
+            )
+            Spacer(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small)))
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Delete Category",
+                tint = MaterialTheme.colorScheme.error
             )
         }
     }
