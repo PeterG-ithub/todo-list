091dacf651b9ee4a2898da9a46b835b65ec1a559
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-09-19 13:14:17 -0700

    Add CategoryList composable for displaying categories
    - Implemented `CategoryList` composable to display a list of categories
    using `LazyColumn`.
    - Updated `CategoryItem` to handle visibility and delete actions.

---
+++ app/src/main/java/com/example/todo_list_v1/ui/category/list/CategoryList.kt
@@ -0,0 +1,46 @@
+package com.example.todo_list_v1.ui.category.list
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todo_list_v1.data.category.Category
+import com.example.todo_list_v1.ui.category.item.CategoryItem
+import com.example.todo_list_v1.ui.theme.Todolistv1Theme
+
+@Composable
+fun CategoryList(
+    categories: List<Category>,
+    onCategoryClick: (Category) -> Unit,
+    onDeleteClick: (Category) -> Unit,
+    onVisibilityClick: (Category) -> Unit,
+) {
+    LazyColumn {
+        items(categories) { category ->
+            CategoryItem(
+                category = category,
+                visible = true,
+                modifier = Modifier
+                    .clickable { onCategoryClick(category) }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryListPreview() {
+    Todolistv1Theme {
+        CategoryList(
+            categories = listOf(
+                Category(name = "Category 1"),
+                Category(name = "Category 2")
+            ),
+            onCategoryClick = { },
+            onDeleteClick = { },
+            onVisibilityClick = { }
+        )
+    }
+}
