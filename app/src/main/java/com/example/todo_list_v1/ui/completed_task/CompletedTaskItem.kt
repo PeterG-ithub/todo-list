e331e10005efdc6219cb75ea1f811388372f6f48
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-09-18 18:35:40 -0700

    Update CompletedTaskItem UI

--- app/src/main/java/com/example/todo_list_v1/ui/completed_task/CompletedTaskItem.kt
+++ app/src/main/java/com/example/todo_list_v1/ui/completed_task/CompletedTaskItem.kt
@@ -1,5 +1,6 @@
 package com.example.todo_list_v1.ui.completed_task
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -7,12 +8,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -21,33 +26,70 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.todo_list_v1.R
 import com.example.todo_list_v1.data.completed_task.CompletedTask
+import com.example.todo_list_v1.ui.task.entry.convertMillisToDate
 import com.example.todo_list_v1.ui.theme.Todolistv1Theme
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Composable
 fun CompletedTaskItem(completedTask: CompletedTask) {
-    // Display task details, customize this based on your UI requirements
-    Card(
-        elevation = CardDefaults.cardElevation(defaultElevation = 1.dp)
+    // Format the completion time
+    val dateFormat = remember { SimpleDateFormat("h:mm a", Locale.getDefault()) }
+    val formattedCompletionTime = dateFormat.format(Date(completedTask.completedAt))
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(id = R.dimen.padding_small)),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Row(
+        // Display the formatted completion time
+        Text(
+            text = formattedCompletionTime,
+            style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(dimensionResource(id = R.dimen.padding_small)),
-            verticalAlignment = Alignment.CenterVertically
+                .padding(end = dimensionResource(id = R.dimen.padding_small))
+        )
+        // Card containing task details
+        Card(
+            elevation = CardDefaults.cardElevation(defaultElevation = 1.dp),
+            modifier = Modifier.weight(1f) // Allow card to take remaining space
         ) {
-            Checkbox(
-                checked = true,
-                onCheckedChange = {},
-                enabled = false
-            )
-            Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.padding_small)))
-            Column {
-                Text(
-                    text = completedTask.taskName,
-                    style = MaterialTheme.typography.bodyLarge,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    modifier = Modifier.widthIn(max = 150.dp)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(id = R.dimen.padding_small)),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = true,
+                    onCheckedChange = {},
+                    enabled = false
+                )
+                Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.padding_small)))
+                Column {
+                    Text(
+                        text = completedTask.taskName,
+                        style = MaterialTheme.typography.bodyLarge,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.widthIn(max = 150.dp)
+                    )
+                    if (completedTask.taskDueDate != null) {
+                        Text(
+                            text = convertMillisToDate(completedTask.taskDueDate),
+                            style = MaterialTheme.typography.labelSmall,
+                            color = MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.5f)
+                        )
+                    }
+                }
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "More Options",
+                    modifier = Modifier
+                        .clickable {  }
                 )
             }
         }
@@ -61,7 +103,8 @@ fun CompletedTaskItemPreview() {
         CompletedTaskItem(
             completedTask = CompletedTask(
                 taskName = "11",
-                completedAt = 1726691218190
+                completedAt = 1726691218190,
+                taskDueDate = 1726691218190
             )
         )
     }
--- app/src/main/java/com/example/todo_list_v1/ui/completed_task/CompletedTaskScreen.kt
+++ app/src/main/java/com/example/todo_list_v1/ui/completed_task/CompletedTaskScreen.kt
@@ -1,20 +1,15 @@
 package com.example.todo_list_v1.ui.completed_task
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -24,12 +19,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.todo_list_v1.R
 import com.example.todo_list_v1.TodoTopAppBar
 import com.example.todo_list_v1.data.completed_task.CompletedTask
-import com.example.todo_list_v1.data.completed_task.CompletedTaskRepository
 import com.example.todo_list_v1.ui.AppViewModelProvider
 import com.example.todo_list_v1.ui.home.CompletedTaskRepositoryMock
 import com.example.todo_list_v1.ui.navigation.NavigationDestination
@@ -98,36 +91,35 @@ fun CompletedTaskScreen(
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun CompletedTaskScreenPreview() {
     Todolistv1Theme {
         CompletedTaskScreen(
             categoryId = null,
-            navigateBack = { /*TODO*/ },
-            onNavigateUp = { /*TODO*/ },
+            navigateBack = { /* Handle navigation back */ },
+            onNavigateUp = { /* Handle navigate up */ },
             viewModel = CompletedTaskViewModel(
                 CompletedTaskRepositoryMock(listOf(
-                        CompletedTask(
-                            id = 1,
-                            taskId = 1,
-                            taskName = "Completed Task 1",
-                            taskDescription = "This is the first completed task.",
-                            taskCategory = "Work",
-                            taskDueDate = null,
-                            completedAt = System.currentTimeMillis()
-                        ),
+                    CompletedTask(
+                        id = 1,
+                        taskId = 1,
+                        taskName = "Completed Task 1",
+                        taskDescription = "This is the first completed task.",
+                        taskCategory = "1", // Assuming category ID is represented as a String
+                        taskDueDate = null,
+                        completedAt = System.currentTimeMillis() - 3600000 // Example timestamp
+                    ),
                     CompletedTask(
                         id = 2,
                         taskId = 2,
                         taskName = "Completed Task 2",
                         taskDescription = "This is the second completed task.",
-                        taskCategory = "Personal",
+                        taskCategory = "2", // Assuming category ID is represented as a String
                         taskDueDate = null,
-                        completedAt = System.currentTimeMillis()
+                        completedAt = System.currentTimeMillis() - 7200000 // Example timestamp
                     )
-                )
-                )
+                ))
             )
         )
     }
